59e9f83760b89ec4594dcf9bf937b7430f53d592
Rafi <rafi@Rafis-MacBook-Air.local>
2026-09-26 10:52:42 +0700

    Add new Vercel domain to CORS

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -29,6 +29,7 @@ class Settings(BaseSettings):
         "https://financial-manager-flax.vercel.app",
         "https://financial-manager-nu.vercel.app",
         "https://financial-manager-inky.vercel.app",
+        "https://financial-manager-kwi4rf5e7-fin-pro2.vercel.app",
         "https://financial-manager-production-a042.up.railway.app",
     ]
     
