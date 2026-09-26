fde7b5fbe68325bc876d42f59bc571ffd446886c
Rafi <rafi@Rafis-MacBook-Air.local>
2026-09-26 19:49:11 +0700

    Fix Settings - remove double /api prefix

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -84,7 +84,7 @@ const ProfileTab = ({ user }) => {
     setMessage('');
     
     try {
-      const response = await api.request('/api/me', {
+      const response = await api.request('/me', {
         method: 'PUT',
         body: JSON.stringify({
           full_name: form.full_name,
@@ -167,7 +167,7 @@ const SecurityTab = () => {
     setMessage('');
 
     try {
-      const response = await api.request('/api/change-password', {
+      const response = await api.request('/change-password', {
         method: 'POST',
         body: JSON.stringify({
           old_password: form.current_password,
